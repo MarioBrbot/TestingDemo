663a996355139298dddf2969f4b6a9e6f9647d89
MarioBrbot <mariobrbot@gmail.com>
2023-11-20 09:56:08 +0100

    Testing Dividing By Zero, LoadingText File

--- DemoLibrary.Tests/CalculatorTests.cs
+++ DemoLibrary.Tests/CalculatorTests.cs
@@ -15,15 +15,37 @@ namespace DemoLibrary.Tests
         [InlineData(21, 5.25, 26.25)]
         [InlineData(double.MaxValue, 5.25, double.MaxValue)]
         public void Add_SimpleValuesShouldCalculate(double x, double y, double expected) 
-        {
-         
-
+        {   
             double actual = Calculator.Add(x, y);
 
-            Assert.Equal(expected, actual);
-        
-        
+            Assert.Equal(expected, actual);             
         }
+
+
+
+
+        [Theory]
+        [InlineData(8, 4, 2)]
+        public void Divide_SimpleValuesShouldCalculate(double x, double y, double expected)
+        { 
+            double actual = Calculator.Divide(x, y);
+
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Fact]
+        public void Divide_DivideByZero()
+        {
+            double expected = 0;
+
+            double actual = Calculator.Divide(15, 0);
+
+            Assert.Equal(expected, actual);
+        }
+
+
+
 
 
     }
---
+++ DemoLibrary.Tests/ExamplesTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using DemoLibrary;
+
+namespace DemoLibrary.Tests
+{
+    public class ExamplesTests
+    {
+        [Fact]
+        public void ExampleLoadTextFile_ValidNameShouldWork()
+        {
+            string actual = Examples.ExampleLoadTextFile("This is a valid file name");
+
+            Assert.True(actual.Length > 0);
+        }
+    }
+}
