8ab6183fa2e6403d052b76a05e6cae431bbc2558
MarioBrbot <mariobrbot@gmail.com>
2023-11-20 09:20:23 +0100

    Setting Up Test Project And Add Test
    Created a new class library project, added nuget xUnit to it. Made a simple test for Add feature of our calculator.

---
+++ DemoLibrary.Tests/CalculatorTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoLibrary;
+using Xunit;
+
+namespace DemoLibrary.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Add_SimpleValuesShouldCalculate() 
+        {
+            double expected = 5;
+
+            double actual = Calculator.Add(3, 2);
+
+            Assert.Equal(expected, actual);
+        
+        
+        }
+
+
+    }
+}
